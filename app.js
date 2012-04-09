5d8694bba9e62401520e82ee73d424fbbf0bc7b4
Dave Ward <dave@encosia.com>
2012-04-08 23:03:10 -0400

    Example script to scrape and echo my site's post headings.

---
+++ app.js
@@ -0,0 +1,10 @@
+var request = require('request'),
+		cheerio = require('cheerio');
+
+request('http://encosia.com', function(error, response, body) {
+	var $ = cheerio.load(body);
+
+	$('h2').each(function() {
+		console.log($(this).text());
+	});
+});
